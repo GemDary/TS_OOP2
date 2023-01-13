e2bc72b218fedc70f0d01a33cbb4efd2a89dd7f1
GemDary <94610697+GemDary@users.noreply.github.com>
2023-01-13 06:59:18 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 class Resistor {
     r: number = 0;
-    constructor(r: number) {
+    maxPower: number = 0;
+    constructor(r: number, maxPower: number) {
       this.r = r;
+      this.maxPower = maxPower;
     }
     getCurrent(u: number): number {
       return u / this.r;
     }
+    getPower(u: number): number {
+      const current = this.getCurrent(u);
+      return u * current;
+    }
+    checkVoltage(u: number): boolean {
+      return this.getPower(u) <= this.maxPower;
+    }
   }
   
-  let resistors: Resistor[] = [];
-  resistors.push(new Resistor(220));
-  resistors.push(new Resistor(4700));
-  resistors.push(new Resistor(110));
-  let totalCurrent: number = 0;
-  resistors.forEach((resistor) => { totalCurrent += resistor.getCurrent(5); });
-  console.log(totalCurrent);
+  let resistors: Resistor[] = [
+    new Resistor(220, 1),
+    new Resistor(4700, 2),
+    new Resistor(110, 0.5)
+  ];
+  
+  function filterResistorsByVoltage(voltage: number): Resistor[] {
+    return resistors.filter((resistor) => {
+      return resistor.checkVoltage(voltage);
+    });
+  }
+  
+  let filteredResistors = filterResistorsByVoltage(5);
+  console.log(filteredResistors);
